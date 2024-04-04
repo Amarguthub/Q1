020e747544cea66abab212fd0725c055e384c747
Amarguthub <98630520+Amarguthub@users.noreply.github.com>
2024-04-04 13:57:14 +0530

    Create DuplicateCharacters.java

---
+++ DuplicateCharacters.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class DuplicateCharacters {
+    public static void main(String[] args) {
+        String str = "programming";
+        
+
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        
+   
+        for (char c : str.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+        
+
+        System.out.println("Duplicate characters in the string '" + str + "':");
+        for (char c : charCountMap.keySet()) {
+            if (charCountMap.get(c) > 1) {
+                System.out.println(c + ": " + charCountMap.get(c));
+            }
+        }
+    }
+}
